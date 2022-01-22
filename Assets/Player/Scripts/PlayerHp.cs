3cd9cc7a16953e3923b37b1d7c9ef7f74ec0af85
sohyun127 <98076050+sohyun127@users.noreply.github.com>
2022-01-22 22:41:43 +0900

    Revert "Merge branch 'main' of https://github.com/bokyung99/GURU1-project"
    This reverts commit 2d2ed63d0b7acf97d61f4f72797f08f75cc2ad07, reversing
    changes made to 8c322d0e3d07a57da8b362102f33f1c44c467bc6.

--- Assets/Player/Scripts/PlayerHp.cs
+++ Assets/Player/Scripts/PlayerHp.cs
@@ -6,9 +6,9 @@ using UnityEngine.UI;
 public class PlayerHp : MonoBehaviour
 {
     //플레이어 체력 변수 (수정)
-    public int playerHp = 10;
+    public int hp = 10;
     //최대 체력 변수 (수정)
-    int playermaxHp = 10;
+    int maxHp = 10;
     //hp 슬라이더 변수
     public Slider hpSlider;
 
@@ -18,9 +18,9 @@ public class PlayerHp : MonoBehaviour
     void Update()
     {
         //현재 플레이어 hp(%)를 hp 슬라이더의 value에 반영한다.
-        hpSlider.value = (float)playerHp / (float)playermaxHp;
+        hpSlider.value = (float)hp / (float)maxHp;
         //hp가 0이 되면 플레이어 사망, 게임 종료
-        if (playerHp == 0)
+        if (hp == 0)
         {
 
         }
@@ -30,19 +30,19 @@ public class PlayerHp : MonoBehaviour
     public void DamageAction(int damage)
     {
         //에너미의 공격력만큼 플레이어의 체력 감소
-        playerHp -= damage;
+        hp -= damage;
     }
 
     //충돌 감지
     private void OnCollisionEnter(Collision collision)
     {
-        //충돌한 게임 오브젝트의 태그가 HealPack일 때
+        //충돌한 게임 오브젝트의 태그가 healPack일 때
         if (collision.gameObject.tag == "HealPack")
         {
             //hpPack 제거
             Destroy(hpPack);
             //플레이어 체력 회복 (수정)
-            playerHp += 2;
+            hp += 2;
         }
     }
 }
