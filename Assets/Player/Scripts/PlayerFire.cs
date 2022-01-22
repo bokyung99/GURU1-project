8c322d0e3d07a57da8b362102f33f1c44c467bc6
sohyun127 <98076050+sohyun127@users.noreply.github.com>
2022-01-22 22:33:26 +0900

    총 발사, 피격효과, 에임ui [소현]

---
+++ Assets/Player/Scripts/PlayerFire.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+
+    //발사 위치
+    public GameObject firePosition;
+    //투척 무기
+    public GameObject bombFactory;
+
+    //투척 파워
+    public float throwPower = 15f;
+
+    //피격 효과 오브젝트
+    public GameObject P_bulletEffect;
+
+    //피격 효과 파티클 시스템
+    ParticleSystem ps;
+
+    void Start()
+    {
+        //피격 효과 오브젝트에서 파티클 시스템 컴포먼트 가져오기
+        ps = P_bulletEffect.GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        UpdateA();
+;    }
+
+
+    void UpdateA()
+    {
+        //마우스 휠 누르면 수류탄 투척
+        if (Input.GetMouseButtonDown(2))
+        {
+            //수류탄 오브젝트 생성 후 수류탄의 생성위치를 발사 위치로
+            GameObject bomb = Instantiate(bombFactory);
+            bomb.transform.position = firePosition.transform.position;
+
+            //수류탄 오브젝트의 rigidbody컴포넌트 가져오기
+            Rigidbody rb = bomb.GetComponent<Rigidbody>();
+            //카메라정면 방향으로 수류탄에 물리적인 힘 가하기
+            rb.AddForce(Camera.main.transform.forward * throwPower, ForceMode.Impulse);
+
+        }
+
+        //마우스 왼쪽 버튼 누르면 총 발사
+        if(Input.GetMouseButtonDown(0))
+        {
+            //레이를 생성한 후 발사될 위치와 진행 방향을 설정
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+            //레이가 부딪힌 대상의 정보를 저장할 변수를 생성
+            RaycastHit hitInfo = new RaycastHit();
+
+            //레이를 발사한 후 만일 부딪힌 물체가 있으면 피격 효과를 표시
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                //피격 효과이 위치를 레이가 부딪힌 지점으로 이동
+                P_bulletEffect.transform.position = hitInfo.point;
+
+                //피격 효과의 forward방향을 레이가 부딪힌 지점의 법선 벡터와 일치시킴
+                P_bulletEffect.transform.forward = hitInfo.normal;
+              
+                //피격 효과 플레이
+                ps.Play();
+
+            }
+
+        }
+
+    }
+}
