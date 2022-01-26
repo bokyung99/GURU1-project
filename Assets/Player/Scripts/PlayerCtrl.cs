9af2ae779c25c23fd2ccb3d004cab62bfb02e424
bokyung99 <97912032+bokyung99@users.noreply.github.com>
2022-01-26 09:58:16 +0900

    메뉴 마우스 커서, 조작 기능 추가 [보경]
    메뉴 켜지거나 켰을 때, 마우스 조작 기능 추가

--- Assets/Player/Scripts/PlayerCtrl.cs
+++ Assets/Player/Scripts/PlayerCtrl.cs
@@ -7,18 +7,23 @@ public class PlayerCtrl : MonoBehaviour
     //마우스 이동으로 카메라 회전
     private CamPosition cp;
 
-    private void Awake()
-    {
-        // 만약 게임 상태가 '중지'라면 마우스 커서가 보이고 조작 가능하도록
-        // visible = true 코드를 활용하면 캠 전환이 안됨.
-        //마우스 커서 보이지 않게 설정하고 현재 위치에 고정
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-
-        cp = GetComponent<CamPosition>();
-    }
     private void Update()
     {
+        // 만약 게임 상태가 '중지'라면 마우스 커서가 보이고 조작 가능하도록
+        // 게임 상태가 '게임 중'이 아니라면 커서 보이고 조작 가능
+        if (GameManager.gm.gState != GameManager.GameState.Run)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        // visible = true 코드를 활용하면 캠 전환이 안됨.
+        //마우스 커서 보이지 않게 설정하고 현재 위치에 고정
+        else
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            cp = GetComponent<CamPosition>();
+        }
         UpdateRotate();
     }
 
