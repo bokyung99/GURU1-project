e09abb1b87b241c116acd534d86870be0f2525eb
alskzldud <alskzldud@naver.com>
2022-01-27 18:39:12 +0900

    근접 공격

--- Assets/Player/Scripts/PlayerAttack.cs
+++ Assets/Player/Scripts/PlayerAttack.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerAttack : MonoBehaviour
 {
     public GameObject enemy;
-    public float enemyHp;
+    public int enemyHp;
+    public int enemyMaxHp;
+    // 에너미 hp Slider 변수
+    public Slider hpSlider;
 
+    private void Update()
+    {
+        // 현재 hp(%)를 hp 슬라이더의 value에 반영한다.
+        hpSlider.value = (float)enemyHp / (float)enemyMaxHp;
+    }
     private void OnTriggerEnter(Collider other)
     {
         //충돌한 게임 오브젝트의 태그가 Enemy일 때
@@ -15,16 +24,8 @@ public class PlayerAttack : MonoBehaviour
             //키보드 T를 눌렀을 때 (수정)
             if (Input.GetKeyDown(KeyCode.T))
             {
-                //StartCoroutine(EnemyHit());
+                enemyHp --;
             }
         }
     }
-    /*
-    IEnumerator EnemyHit()
-    {
-        GameObject.Find("Enemy").GetComponent<EnemyFSM>();
-        
-
-    }
-    */
 }
